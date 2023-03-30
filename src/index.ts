98e0f4fce3cbf2cfeea986cbd7e323aaeb86ecbb
TinkMingKing <294248306@qq.com>
2023-03-30 16:41:15 +0800

    add command

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Plugin, clientApi} from 'siyuan';
+import { Plugin, clientApi } from 'siyuan';
 import { main } from './createIndex'
 
 export default class IndexPlugin extends Plugin {
@@ -9,19 +9,26 @@ export default class IndexPlugin extends Plugin {
     }
 
     //插入按钮
-    insertIcon(){
+    insertIcon() {
         this.el = document.createElement('div');
         this.el.classList.add('toolbar__item', 'b3-tooltips', 'b3-tooltips__se');
         this.el.setAttribute('aria-label', '插入目录');
         this.el.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 12H3"></path><path d="M16 6H3"></path><path d="M16 18H3"></path><path d="M18 9v6"></path><path d="M21 12h-6"></path></svg>';
-        this.el.addEventListener('click',() => {
+        this.el.addEventListener('click', () => {
             main();
         });
         clientApi.addToolbarRight(this.el);
     }
 
     onload() {
-        this.insertIcon();   
+        this.insertIcon();
+        this.registerCommand({
+            command: 'ic',
+            shortcut: 'ctrl+alt+i,command+option+e',
+            description: '插入目录',
+            callback: main
+
+        });
     }
 
     onunload() {
